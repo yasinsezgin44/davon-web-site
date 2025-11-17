9de111621ed86067db0b3a123b873203ffb09d97
yasinsez <yasinsezgin4402@gmail.com>
2025-11-17 10:21:37 +0300

    Delete the old implemtation to start new codes.

--- src/app/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as CornerCTA } from './CornerCTA';
-export { default as CustomCursor } from './CustomCursor';
-export { default as Header } from './Header';
-export { default as Hero } from './Hero';
-export { useScrollAnimation } from './useScrollAnimation';
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import { CornerCTA, Header, Hero } from "./components";
-
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-[var(--bg-100)] font-inter text-[var(--text-100)] transition-all duration-1000">
-      <CornerCTA />
-      <Header />
-      <Hero />
-    </div>
-  );
+  return <div></div>;
 }
